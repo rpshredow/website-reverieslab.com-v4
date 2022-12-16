f5ea0f40905332282648a7c375c73f6c847504ea
rpshredow <rshredow@gmail.com>
2022-12-15 20:12:56 -0800

    added js templates

---
+++ templates.js
@@ -0,0 +1,81 @@
+class MyHeader extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+            <header class="header">
+                <a href="index.html">
+                    <img class="logo" src="/logo.webp" alt="Reveries Lab Logo" />
+                </a>
+                <nav class="main-nav">
+                    <ul class="main-nav-list">
+                    <li><a class="main-nav-link" href="/index.html">Home</a></li>
+                    <li>
+                        <a class="main-nav-link" href="/projects/index.html">Projects</a>
+                    </li>
+                    <li>
+                        <a class="main-nav-link" href="/research/index.html">Research</a>
+                    </li>
+                    <li><a class="main-nav-link" href="/about.html">About</a></li>
+                    </ul>
+                </nav>
+            </header>
+        `;
+  }
+}
+
+customElements.define("my-header", MyHeader);
+
+class MyFooter extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+            <footer class="footer">
+                <div class="container grid-footer">
+                <div class="footer-socials">
+                    <ul class="social-links">
+                    <li>
+                        <a class="footer-link" href="https://www.instagram.com/rshredow/"
+                        ><ion-icon class="social-icon" name="logo-instagram"></ion-icon
+                        ></a>
+                    </li>
+                    <li>
+                        <a
+                        class="footer-link"
+                        href="https://www.facebook.com/robert.shredow"
+                        ><ion-icon class="social-icon" name="logo-facebook"></ion-icon
+                        ></a>
+                    </li>
+                    <li>
+                        <a class="footer-link" href="https://github.com/rpshredow"
+                        ><ion-icon class="social-icon" name="logo-github"></ion-icon
+                        ></a>
+                    </li>
+                    <li>
+                        <a
+                        class="footer-link"
+                        href="https://www.linkedin.com/in/robert-shredow-37995393/"
+                        ><ion-icon class="social-icon" name="logo-linkedin"></ion-icon
+                        ></a>
+                    </li>
+                    </ul>
+                </div>
+                <div class="footer-col">
+                    <p class="footer-address">
+                    111 College Hill St, Cheney Washington 99004
+                    </p>
+                    <a class="footer-link" href="mailto:rshredow@gmail.com"
+                    >rshredow@gmail.com</a
+                    >
+                    <a class="footer-link" href="tel:509-294-5271">509-294-5271</a>
+                </div>
+
+                <div class="footer-col copyright-span">
+                    <p class="footer-copyright">
+                    Copyright &copy; 2022 by Robert Shredow, All rights reserved.
+                    </p>
+                </div>
+                </div>
+            </footer>
+        `;
+  }
+}
+
+customElements.define("my-footer", MyFooter);
